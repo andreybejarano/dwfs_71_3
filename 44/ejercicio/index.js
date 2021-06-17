728306cb49fd5c82360c10f4e59d84f83d03f5eb
Vera177 <veradeviaje@gmail.com>
2021-06-16 22:59:57 -0300

    endpoints funcionando

--- 44/ejercicio/index.js
+++ 44/ejercicio/index.js
@@ -2,10 +2,41 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const users = [{
+    user: 'juan',
+    password: '123456',
+    country: 'AR'
+}];
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static('public'));
+
+app.post('/login', (req, res) => {
+    const user = users.find(element => element.user === req.body.user && element.password === req.body.password);
+    if (!user) {
+        return res.status(401).json({
+            status: 401,
+            error: 'user/password invalid'
+        })
+    }
+    delete user.password;
+    return res.json(user);
+})
+
+app.post('/user', (req, res) => {
+    const {user,password,country} = req.body;
+    users.push({user,password,country});
+    return res.status(201).json({
+        status: 201,
+        message: "user created"
+    })
+})
+
+app.get('/user', (req, res) => {
+    return res.json(users);
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`server started on port ${process.env.PORT}`)
