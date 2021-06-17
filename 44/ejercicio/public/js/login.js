76a8b0dbcbd772fdb83308ea65c2c2f4c9303189
Belen Korobka <belenkorobka@gmail.com>
2021-06-16 23:36:21 -0300

    Integración pantalla login con endpoint login

---
+++ 44/ejercicio/public/js/login.js
@@ -0,0 +1,48 @@
+const txtUser = document.getElementById('username');
+const txtPassword = document.getElementById('password');
+const btnSubmit = document.getElementById('submit');
+
+async function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      const error = {
+        status: response.status,
+        statusText: response.statusText,
+        response: await response.json()
+      };
+      throw error;
+    }
+  }
+  
+  const request =  (url, options = {}) => {
+    options.credentials = 'include';
+    options.mode = 'cors';
+    options.cache = 'default';
+    options.body = JSON.stringify(options.body);
+    options.headers = {
+      ...options.headers,
+      'Content-Type': 'application/json'
+    };
+  
+    return new Promise((resolve, reject) => {
+      fetch(url, options)
+        .then(checkStatus)
+        .then(response => response.json())
+        .then(response => resolve(response))
+        .catch((error) => reject(error));
+    });
+  };
+  
+
+btnSubmit.addEventListener('click', (e) => {
+    e.preventDefault();
+    request('/login', {
+        method: 'POST',
+        body: {user: txtUser.value, password: txtPassword.value}
+    }).then(
+        res => {
+            console.log(res);
+        }
+    );
+})
