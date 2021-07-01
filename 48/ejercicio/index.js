7cb2127efdfdde4dc7c61c59b625bd2482f961e3
Vera177 <veradeviaje@gmail.com>
2021-06-30 23:35:03 -0300

    crear modelo songs

--- 48/ejercicio/index.js
+++ 48/ejercicio/index.js
@@ -3,12 +3,28 @@ const express = require('express');
 
 const connection = require('./connection');
 
+const songsModel = require('./songsModel');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/song', async (req, res) => {
+    try {
+        const songs = await songsModel.findAll();
+        return res.json({
+            status: 200,
+            data: songs
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            error
+        });
+    }
 
+});
 
 
 const port = process.env.PORT;
