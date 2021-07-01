c6e2b3560de8369028c596f3a1f0cc50f94c8e66
Roberto Reina <robertto.e@gmail.com>
2021-06-30 22:23:52 -0400

    agregar modelo albums

--- 48/ejercicio/index.js
+++ 48/ejercicio/index.js
@@ -2,12 +2,29 @@ require('dotenv').config();
 const express = require('express');
 
 const connection = require('./connection');
+const albumsModel = require('./albumsModels');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Get albums
+app.get('/album', async (req, res) =>{
+    try {
+        const albums = await albumsModel.findAll();
+        return res.json({
+            status:200,
+            data: albums
+        })
+    
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            error
+        })
+    }
+});
 
 
 
