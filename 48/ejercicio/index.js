d55e84b9c35b9b810d5f93c228ef267a691ad0e6
Belen Korobka <belenkorobka@gmail.com>
2021-06-30 23:26:29 -0300

    crear model band

--- 48/ejercicio/index.js
+++ 48/ejercicio/index.js
@@ -2,13 +2,27 @@ require('dotenv').config();
 const express = require('express');
 
 const connection = require('./connection');
+const bandModel = require('./bandModel');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+app.get('/band', async (req, res) => {
+    try {
+        const bands = await bandModel.findAll();
+        return res.json({
+            status: 200,
+            data: bands
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: 500,
+            error
+        })
+    }
+});
 
 
 const port = process.env.PORT;
