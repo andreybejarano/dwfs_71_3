73bcabc6f87fb4008abe8b14774d61e5528c2603
Andrey Bejarano <andreybejarano@gmail.com>
2021-07-28 22:04:16 -0300

    encriptar y validar contraseña en el modelo usuario

--- data_warehouse/models/user.js
+++ data_warehouse/models/user.js
@@ -1,11 +1,27 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const schema = mongoose.Schema({
-    firstname: { type: String, require: true},
-    lastname: { type: String, require: true},
-    email: { type: String, require: true},
-    password: { type: String, require: true},
-    roles_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Roles'}
+    firstname: { type: String, require: true },
+    lastname: { type: String, require: true },
+    email: { type: String, require: true },
+    password: { type: String, require: true },
+    roles_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Roles' }
 });
+
+schema.pre('save', function (next) {
+    if (!this.isModified('password')) {
+        return next();
+    }
+    const salt = bcrypt.genSaltSync(10);
+    this.password = bcrypt.hashSync(this.password, salt);
+    next();
+});
+
+schema.methods.comparePassword = function (plaintext) {
+    return new Promise((resolve, reject) => {
+        return resolve(bcrypt.compareSync(plaintext, this.password));
+    });
+}
 
 module.exports = mongoose.model('Users', schema);
